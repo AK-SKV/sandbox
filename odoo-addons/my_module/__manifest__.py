b9ab69cd552b141936f4f7374d3bb527c2c9938a
fernando <fernando.haro@openapps.mx>
2018-07-14 21:00:50 -0600

    Change on manifest

--- odoo-addons/my_module/__manifest__.py
+++ odoo-addons/my_module/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "Construction 4.0",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Construction features for Odoo 11""",
 
     'description': """
-        Long description of module's purpose
+        This module include some new features
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Directiva Digital",
+    'website': "http://www.directivadigital.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Project',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
